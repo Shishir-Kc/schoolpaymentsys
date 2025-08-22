3f102fd9f6dc7c4b4bab918b2c26340411694cbf
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-08-22 20:51:50 +0545

    added payment_db view

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Custom_user,Payment
 
-# Register your models here.
+admin.site.register(Custom_user)
+class PAYMENT_ADMIN(admin.ModelAdmin):
+    list_display=[
+        'user',
+        'purchase_id',
+        'purchse_status',
+        'purchase_amount',
+        'khalti_status',
+        'pidx',
+        'khalti_transaction_id',
+        'initiated_at',
+        'updated_at'
+    ]
+admin.site.register(Payment,PAYMENT_ADMIN)
