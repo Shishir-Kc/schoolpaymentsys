c899086ebbe855de2e5a29b244867837564ef478
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-08-23 08:45:15 +0545

    added * for al host

--- SCHOOL/settings.py
+++ SCHOOL/settings.py
@@ -25,7 +25,7 @@ AUTH_USER_MODEL = "home.Custom_user"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
